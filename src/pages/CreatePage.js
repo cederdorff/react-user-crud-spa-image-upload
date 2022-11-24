eb6b12e3e6256d5d12e2c49e0044b47f84c70cfb
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-24 16:52:22 +0100

    image upload added

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import imgPlaceholder from "../assets/img/img-placeholder.jpg";
 
 export default function CreatePage() {
     const navigate = useNavigate();
@@ -28,6 +29,25 @@ export default function CreatePage() {
         }
     }
 
+    /**
+     * handleImageChange is called every time the user chooses an image in the fire system.
+     * The event is fired by the input file field in the form
+     */
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+        if (file.size < 500000) {
+            // image file size must be below 0,5MB
+            const reader = new FileReader();
+            reader.onload = event => {
+                setImage(event.target.result);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            // if not below 0.5MB display an error message using the errorMessage state
+            console.log("The image file is too big!");
+        }
+    }
+
     return (
         <section className="page">
             <h1>Create New User</h1>
@@ -35,8 +55,10 @@ export default function CreatePage() {
                 <input type="text" value={name} placeholder="Type a name" onChange={e => setName(e.target.value)} />
                 <input type="text" value={title} placeholder="Type a title" onChange={e => setTitle(e.target.value)} />
                 <input type="mail" value={mail} placeholder="Type a mail" onChange={e => setMail(e.target.value)} />
-                <input type="url" value={image} placeholder="Paste image url" onChange={e => setImage(e.target.value)} />
-                {image && <img className="image-preview" src={image} alt="Choose" />}
+                <label>
+                    <input type="file" className="file-input" accept="image/*" onChange={handleImageChange} />
+                    <img className="image-preview" src={image} alt="Choose" onError={event => (event.target.src = imgPlaceholder)} />
+                </label>
                 <button>Save</button>
             </form>
         </section>
--- src/pages/UpdatePage.js
+++ src/pages/UpdatePage.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import imgPlaceholder from "../assets/img/img-placeholder.jpg";
 
 export default function UpdatePage() {
     const params = useParams();
@@ -42,6 +43,25 @@ export default function UpdatePage() {
         }
     }
 
+    /**
+     * handleImageChange is called every time the user chooses an image in the fire system.
+     * The event is fired by the input file field in the form
+     */
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+        if (file.size < 500000) {
+            // image file size must be below 0,5MB
+            const reader = new FileReader();
+            reader.onload = event => {
+                setImage(event.target.result);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            // if not below 0.5MB display an error message using the errorMessage state
+            console.log("The image file is too big!");
+        }
+    }
+
     return (
         <section className="page">
             <h1>Update "{name}"</h1>
@@ -49,8 +69,10 @@ export default function UpdatePage() {
                 <input type="text" value={name} placeholder="Type a name" onChange={e => setName(e.target.value)} />
                 <input type="text" value={title} placeholder="Type a title" onChange={e => setTitle(e.target.value)} />
                 <input type="mail" value={mail} placeholder="Type a mail" onChange={e => setMail(e.target.value)} />
-                <input type="url" value={image} placeholder="Paste image url" onChange={e => setImage(e.target.value)} />
-                {image && <img className="image-preview" src={image} alt="Choose" />}
+                <label>
+                    <input type="file" className="file-input" accept="image/*" onChange={handleImageChange} />
+                    <img className="image-preview" src={image} alt="Choose" onError={event => (event.target.src = imgPlaceholder)} />
+                </label>
                 <button>Save</button>
             </form>
         </section>
